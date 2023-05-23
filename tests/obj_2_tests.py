24efbdb513070e121c45e5012aa90b0976bc0b53
edsml-jm4622 <jm4622@ic.ac.uk>
2023-05-23 10:50:03 +0100

    added conv vae tests

---
+++ tests/obj_2_tests.py
@@ -0,0 +1,59 @@
+import torch
+import pytest
+from ..cVAE import VAE_Encoder_Conv, VAE_Decoder_Conv, VAE_Conv
+
+@pytest.fixture
+def vae_encoder_conv():
+    return VAE_Encoder_Conv()
+
+@pytest.fixture
+def vae_decoder_conv():
+    return VAE_Decoder_Conv()
+
+@pytest.fixture
+def vae_conv():
+    device = 'cpu'
+    return VAE_Conv(device)
+
+def test_vae_encoder_conv_forward(vae_encoder_conv):
+    batch_size = 10
+    channels = 1
+    height = 28
+    width = 28
+    x = torch.randn(batch_size, channels, height, width)
+    mu, sigma = vae_encoder_conv(x)
+    assert mu.shape == (batch_size, 120, 16, 16)
+    assert sigma.shape == (batch_size, 120, 16, 16)
+
+def test_vae_decoder_conv_forward(vae_decoder_conv):
+    batch_size = 10
+    latent_channels = 120
+    latent_height = 16
+    latent_width = 16
+    x = torch.randn(batch_size, latent_channels, latent_height, latent_width)
+    output = vae_decoder_conv(x)
+    assert output.shape == (batch_size, 1, 256, 256)
+
+def test_vae_conv_sample_latent_space(vae_conv):
+    batch_size = 10
+    latent_channels = 120
+    latent_height = 16
+    latent_width = 16
+    mu = torch.randn(batch_size, latent_channels, latent_height, latent_width)
+    sigma = torch.randn(batch_size, latent_channels, latent_height, latent_width)
+    z, kl_div = vae_conv.sample_latent_space(mu, sigma)
+    assert z.shape == (batch_size, latent_channels, latent_height, latent_width)
+    assert kl_div.shape == ()
+
+def test_vae_conv_forward(vae_conv):
+    batch_size = 10
+    channels = 1
+    height = 28
+    width = 28
+    x = torch.randn(batch_size, channels, height, width)
+    z, kl_div = vae_conv(x)
+    assert z.shape == (batch_size, 1, 256, 256)
+    assert kl_div.shape == ()
+
+if __name__ == '__main__':
+    pytest.main()
