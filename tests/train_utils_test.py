a2ac51a0976315b546cafbb4d531332d60f7c3f1
edsml-jm4622 <jm4622@ic.ac.uk>
2023-05-24 17:59:46 +0100

    more tests

--- tests/train_utils_test.py
+++ tests/train_utils_test.py
@@ -9,15 +9,21 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 from wildfire.utils.train import split, create_pairs, create_dataloader, train  # noqa
 
 
-def test_split():
-    arr = np.array([1, 2, 3, 4, 5])
+@pytest.fixture
+def sample_data():
+    arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    return arr
+
+
+def test_split(sample_data):
+    arr = sample_data[:6]
     chunk_size = 2
     expected_result = np.array([1, 2, 3, 4, 5])
     assert np.concatenate(split(arr, chunk_size), axis=0).all() ==  expected_result.all() # noqa
 
 
-def test_create_pairs():
-    arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+def test_create_pairs(sample_data):
+    arr = sample_data
     chunk_size = 2
     expected_result = np.array([1, 3, 5, 7, 9, 2, 4, 6, 8, 10])
     assert np.concatenate(create_pairs(arr, chunk_size)).all() == expected_result.all()  # noqa
