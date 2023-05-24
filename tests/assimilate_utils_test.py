ff366326e47a6878452dcd63de7aef431509e54b
edsml-jm4622 <jm4622@ic.ac.uk>
2023-05-24 17:37:50 +0100

    are weights small enough?

--- tests/assimilate_utils_test.py
+++ tests/assimilate_utils_test.py
@@ -1,7 +1,7 @@
+import os
+import sys
 import pytest
 import numpy as np
-import sys
-import os
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
--- tests/obj_2_test.py
+++ tests/obj_2_test.py
@@ -1,7 +1,8 @@
-import torch
-import pytest
-import sys
 import os
+import sys
+import pytest
+import torch
+
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
