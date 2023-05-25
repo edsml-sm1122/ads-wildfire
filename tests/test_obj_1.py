9fb2baecdeed407f654296eb820531c4adc5691d
edsml-jm4622 <jm4622@ic.ac.uk>
2023-05-25 16:46:40 +0100

    rename tests

--- tests/test_obj_1.py
+++ tests/test_obj_1.py
@@ -6,7 +6,7 @@ import torch
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
-from wildfire.models import cLSTM # noqa
+from wildfire.models import cLSTM  # noqa
 
 
 @pytest.fixture
@@ -71,8 +71,8 @@ def test_convlstm_forward(convlstm):
     channel = 1
     height = 28
     width = 28
-    input_tensor = torch.Tensor(torch.randn(batch_size, sequence, channel,
-                                            height, width))
+    input_tensor = torch.Tensor(
+        torch.randn(batch_size, sequence, channel, height, width))
     h, res = convlstm.forward(input_tensor, None)
     assert h.shape == (sequence, 16, height, width)
     assert res.shape == (sequence, 1, height, width)
